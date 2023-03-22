36e9d5e05f6b0abd0fcc931bb8b0a87c40470064
justinNguyen <nguyenjustin@ufl.edu>
2023-03-22 01:58:45 -0400

    Main Function and Encode Function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # By: Justin Nguyen
 
 def encode(password):
-
-    return 0
+    encoded = ""
+    for i in range(0, len(password)):
+        if int(password[i]) > 6:
+            encoded += str((int(password[i]) + 3) % 10)
+        else:
+            encoded += str(int(password[i])+3)
+    return str(encoded)
 
 
 def main():
@@ -15,7 +20,8 @@ def main():
         print("3. Quit\n")
         option = int(input("Please enter an option: "))
         if option == 1:
-            password = int(input("Please enter your password to encode: "))
+            password = input("Please enter your password to encode: ")
+            password = encode(password)
             print("Your password has been encoded and stored!\n")
         elif option == 2:
             decode(password)
